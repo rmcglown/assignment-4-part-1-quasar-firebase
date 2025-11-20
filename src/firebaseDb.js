2fdefb259ce323d1fd792685fa6d93b8df75e27e
rmcglown <143294882+rmcglown@users.noreply.github.com>
2025-11-19 22:17:20 -0600

    push

---
+++ src/firebaseDb.js
@@ -0,0 +1,20 @@
+// TODO: Add SDKs for Firebase products that you want to use    
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+//YOUR CONFIG HERE
+apiKey: "AIzaSyD1v7CghydB-uFg7oMZuh8qdzHZ-Ztb69E",
+  authDomain: "qausartodolist.firebaseapp.com",
+  projectId: "qausartodolist",
+  storageBucket: "qausartodolist.firebasestorage.app",
+  messagingSenderId: "677792354120",
+  appId: "1:677792354120:web:1d5de8542fc945f4f06d7d"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export default db
